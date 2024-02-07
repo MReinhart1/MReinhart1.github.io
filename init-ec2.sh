cc592df88526b0704ea82891e4fcd07596dfe3a4
Michael Reinhart <michaelreinhart@macbook-michael.mynetworksettings.com>
2024-02-06 21:18:09 -0500

    run on port 80

--- init-ec2.sh
+++ init-ec2.sh
@@ -1,6 +1,7 @@
 sudo yum install git nodejs npm
+git clone https://github.com/MReinhart1/MReinhart1.github.io.git
 cd MReinhart1.github.io
-echo NEXT_PUBLIC_SITE_URL=https://localhost:3000 > .env.local
+echo NEXT_PUBLIC_SITE_URL=https://localhost:80 > .env.local
 npm install
 npm run build
 npm run start
