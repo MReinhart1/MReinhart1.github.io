79cc0d18d36cdad0ac3b3b77d6f3546fc74f3267
Michael Reinhart <michaelreinhart@macbook-michael.mynetworksettings.com>
2024-02-12 20:39:48 -0500

    add projects

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -18,36 +18,35 @@ const projects = [
     name: 'Multi-national Insurance Platform',
     description:
       'Insurance',
-    link: { href: 'https://github.com/MReinhart1/global-connect-backend', label: 'github.com' },
+    link: { href: '/articles/Global-Connect', label: 'github.com' },
     logo: logoComcast,
   },
   {
     name: 'Facial Recognition for Chrome Passwords',
     description:
       'Chromium project',
-    link: { href: 'https://github.com/Mack6870/Facial-Recognition-Chrome-Extension/tree/main/FacialRecognitionWebcamExtension', label: 'github.com' },
+    link: { href: '/articles/Facial-Recognition', label: 'github.com' },
     logo: logoComcast,
   },
   {
     name: 'Real time video to HTML',
     description:
       'Blocks to Paragraphs',
-    // link: { href: '/articles/Comcast', label: 'Comcast' },
-    link: { href: 'https://github.com/Mack6870/Kinect-Website-Builder', label: 'Comcast' },
+    link: { href: '/articles/Video-to-HTML', label: 'Comcast' },
     logo: logoComcast,
   },
   {
     name: 'AI Phone Bot',
     description:
       'Voya bot to make banking transfers',
-    link: { href: 'https://github.com/MReinhart1/voyaUICallTranscriber', label: 'github.com' },
+    link: { href: '/articles/AI-Phone-Bot', label: 'github.com' },
     logo: logoComcast,
   },
   {
-    name: 'Tinder but for Betting',
+    name: 'SportsVote',
     description:
-      'https://github.com/MReinhart1/sportsvoteMVP',
-    link: { href: '#', label: 'github.com' },
+      'SportsVote',
+    link: { href: '/articles/SportsVote', label: 'github.com' },
     logo: logoComcast,
   },
 ]
@@ -65,14 +64,14 @@ function LinkIcon(props) {
 
 export const metadata = {
   title: 'Projects',
-  description: 'Things I’ve made trying to put my dent in the universe.',
+  description: 'Things I have Built.',
 }
 
 export default function Projects() {
   return (
     <SimpleLayout
-      title="Things I’ve made trying to put my dent in the universe."
-      intro="I’ve worked on tons of little projects over the years but these are the ones that I’m most proud of. Many of them are open-source, so if you see something that piques your interest, check out the code and contribute if you have ideas for how it can be improved."
+      title="Things I have Built."
+      intro="I’ve worked on tons of little projects over the years but these are the ones that I’m most proud of. Not all of them here ended up being successful but I still had fun doing each and every one."
     >
       <ul
         role="list"
