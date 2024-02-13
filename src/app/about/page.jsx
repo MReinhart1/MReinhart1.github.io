48caaaecd2c4620f452a12dbd22b81c47b1eecfd
Michael Reinhart <michaelreinhart@macbook-michael.mynetworksettings.com>
2024-02-12 19:25:16 -0500

    add pictures

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -37,7 +37,7 @@ function MailIcon(props) {
 export const metadata = {
   title: 'About',
   description:
-    'I’m Michael Reinhart. I live in New York City, where I design the future.',
+    'I’m Michael Reinhart.  Canadian-American who loves exploring.',
 }
 
 export default function About() {
@@ -56,35 +56,25 @@ export default function About() {
         </div>
         <div className="lg:order-first lg:row-span-2">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
-            I’m Michael Reinhart. I live in New York City, where I design the
-            future.
+            I’m Michael Reinhart.  a Canadian-American who loves exploring.
           </h1>
           <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
             <p>
-              I’ve loved making things for as long as I can remember, and wrote
-              my first program when I was 6 years old, just two weeks after my
-              mom brought home the brand new Macintosh LC 550 that I taught
-              myself to type on.
+              I was born in Oakville Ontario, a suburb of Toronto, and moved to Connecticut when I was about two. Growing up I
+              was obsessed with reading Greek and Norse Mythology, as well as climbing trees. I spent much of my time running 
+              around the forrest behind my home after school, building a tree fort with my Father as well as bows and arrows 
+              out of wooden dowels from Home Depot (until I almost lost my eye).
             </p>
             <p>
-              The only thing I loved more than computers as a kid was space.
-              When I was 8, I climbed the 40-foot oak tree at the back of our
-              yard while wearing my older sister’s motorcycle helmet, counted
-              down from three, and jumped — hoping the tree was tall enough that
-              with just a bit of momentum I’d be able to get to orbit.
+              As I grew older I started to play with computers. I started out with simple things like programming the steps 
+              for a dot to escape a maze using functions: moveLeft(), moveRight(), moveForward(). Then my Father bought me a raspberry 
+              pi for christmas. It was a <a href="https://www.raspberrypi.com/products/raspberry-pi-3-model-b/">Model 3 </a> 
+              where I learned about basic bash scripting and python; as well as basic networking within a Local Area Network.
             </p>
             <p>
-              I spent the next few summers indoors working on a rocket design,
-              while I recovered from the multiple surgeries it took to fix my
-              badly broken legs. It took nine iterations, but when I was 15 I
-              sent my dad’s Blackberry into orbit and was able to transmit a
-              photo back down to our family computer from space.
-            </p>
-            <p>
-              Today, I’m the founder of Planetaria, where we’re working on
-              civilian space suits and manned shuttle kits you can assemble at
-              home so that the next generation of kids really <em>can</em> make
-              it to orbit — from the comfort of their own backyards.
+              Computer Science as a degree was an easy choice for me, I learned all about object oriented programming, networking,
+              discrete math and sorted an array far too many times. I also built out some great pet projects which you should check out
+              on the projects page!
             </p>
           </div>
         </div>
